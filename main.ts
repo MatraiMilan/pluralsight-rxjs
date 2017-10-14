b27e45b50f453740474fa9fdf73941a596ad8c4a
MatraiMilan <matrai.milan89@gmail.com>
2017-10-14 22:06:38 +0200

    create own observer instead of using from function

--- main.ts
+++ main.ts
@@ -1,9 +1,26 @@
-import {Observable, Observer} from "rxjs";
+import {Observable/*, Observer*/} from "rxjs";
 
 let numbers = [1, 5, 10];
-let source = Observable.from(numbers);
+//CAN CREATE OBSERVABLE FROM AN ARRAY FOR EXAMPLE
+//let source = Observable.from(numbers);
 
-source.subscribe(
+//CREATE OWN OBSERVABLE
+let source2 = Observable.create(observer => {
+    for(let number of numbers) {
+        observer.next(number);
+    }
+
+    observer.complete();
+});
+
+/*source.subscribe(
+    // I CAN USE ONLY THE FIRST CALLBACK PARAMETER FUNCTION (THE SECOND AND THIRD CALLBACK FUNCTION IMPLEMENTATION IS NOT NEEDED)
+    value => console.log(`value: ${value}`),
+    error => console.log(`error: ${error}`),
+    () => console.log('complete')
+);*/
+
+source2.subscribe(
     // I CAN USE ONLY THE FIRST CALLBACK PARAMETER FUNCTION (THE SECOND AND THIRD CALLBACK FUNCTION IMPLEMENTATION IS NOT NEEDED)
     value => console.log(`value: ${value}`),
     error => console.log(`error: ${error}`),
