eb244330916b4056347f7f6ee4d8c645a7469d39
MatraiMilan <matrai.milan89@gmail.com>
2017-10-14 21:58:56 +0200

    shorten the subscription

--- main.ts
+++ main.ts
@@ -3,7 +3,14 @@ import {Observable, Observer} from "rxjs";
 let numbers = [1, 5, 10];
 let source = Observable.from(numbers);
 
-class MyObserver implements Observer<number> {
+source.subscribe(
+    // I CAN USE ONLY THE FIRST CALLBACK PARAMETER FUNCTION (THE SECOND AND THIRD CALLBACK FUNCTION IMPLEMENTATION IS NOT NEEDED)
+    value => console.log(`value: ${value}`),
+    error => console.log(`error: ${error}`),
+    () => console.log('complete')
+);
+
+/*class MyObserver implements Observer<number> {
 
     next(value) {
         console.log(`value: ${value}`);
@@ -19,4 +26,4 @@ class MyObserver implements Observer<number> {
     }
 }
 
-source.subscribe(new MyObserver());
+source.subscribe(new MyObserver());*/
