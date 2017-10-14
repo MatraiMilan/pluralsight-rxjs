623fd66f49196607840d6808a29536a8cf71a63c
MatraiMilan <matrai.milan89@gmail.com>
2017-10-14 22:45:37 +0200

    module one is finished

--- main.ts
+++ main.ts
@@ -1,17 +1,38 @@
 import {Observable/*, Observer*/} from "rxjs";
 
+//YOU CAN REDUCE SIGNIFICANT AMOUNT OF KB-S OF DATA IF YOU IMPORT ONLY WHAT YOU USE
+/*import {Observable} from "rxjs/Observable";
+import "rxjs/add/operator/map";
+import "rxjs/add/operator/filter";*/
+
+
 let numbers = [1, 5, 10];
 //CAN CREATE OBSERVABLE FROM AN ARRAY FOR EXAMPLE
 //let source = Observable.from(numbers);
 
 //CREATE OWN OBSERVABLE
 let source2 = Observable.create(observer => {
-    for(let number of numbers) {
-        observer.next(number);
-    }
+    
+    let index = 0;
+    let produceValue = () => {
+       
+        observer.next(numbers[index]);
 
-    observer.complete();
-});
+        index++;
+
+        if(index < numbers.length) {
+            setTimeout(produceValue, 2000);
+        } else {
+            observer.complete();
+        }
+    };
+
+    produceValue();
+    
+})
+    .map(n => n * 2)
+    .filter(n => n > 4)
+;
 
 /*source.subscribe(
     // I CAN USE ONLY THE FIRST CALLBACK PARAMETER FUNCTION (THE SECOND AND THIRD CALLBACK FUNCTION IMPLEMENTATION IS NOT NEEDED)
@@ -22,7 +43,7 @@ let source2 = Observable.create(observer => {
 
 source2.subscribe(
     // I CAN USE ONLY THE FIRST CALLBACK PARAMETER FUNCTION (THE SECOND AND THIRD CALLBACK FUNCTION IMPLEMENTATION IS NOT NEEDED)
-    value => console.log(`value: ${value}`),
+    value => {console.log(`value: ${value}`)},
     error => console.log(`error: ${error}`),
     () => console.log('complete')
 );
