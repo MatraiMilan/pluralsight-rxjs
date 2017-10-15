bec4b9871835cebe34eb547f519d1e6b85b0937e
MatraiMilan <matrai.milan89@gmail.com>
2017-10-15 11:53:29 +0200

    get movies via xmlhttprequest from a local json file

--- main.ts
+++ main.ts
@@ -1,25 +1,29 @@
 import { Observable/*, Observer*/ } from "rxjs";
 
-let circle = document.getElementById("circle");
+let output = document.getElementById("output");
+let button = document.getElementById("button");
 
-let source = Observable.fromEvent(document, "mousemove")
-    .map((event: MouseEvent) => {
-        return {
-            x: event.clientX,
-            y: event.clientY
-        };
-    })
-    .filter(value => value.x < 500)
-    .delay(300);
+let click = Observable.fromEvent(button, "click");
 
+function load(url: string) {
+    let xhr = new XMLHttpRequest();
 
-function nextVal(value) {    
-    circle.style.left = (value.x - 10) + "px";
-    circle.style.top = (value.y - 10) + "px";   
+    xhr.addEventListener("load", () => {
+        let movies = JSON.parse(xhr.responseText);
+
+        movies.forEach(m => {
+            let div = document.createElement("div");
+            div.innerText = m.title;
+            output.appendChild(div);
+        });
+    });
+
+    xhr.open("GET", url);
+    xhr.send();
 }
 
-source.subscribe(
-    nextVal,
+click.subscribe(
+    e => load("movies.json"),
     error => console.log(`error: ${error}`),
     () => console.log('complete')
 );
