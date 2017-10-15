d656f2de803006a81bd0c05cca644979c38ae789
MatraiMilan <matrai.milan89@gmail.com>
2017-10-15 17:43:00 +0200

    use flatmap

--- main.ts
+++ main.ts
@@ -6,24 +6,40 @@ let button = document.getElementById("button");
 let click = Observable.fromEvent(button, "click");
 
 function load(url: string) {
-    let xhr = new XMLHttpRequest();
+    return Observable.create(observer => {
+        let xhr = new XMLHttpRequest();
 
-    xhr.addEventListener("load", () => {
-        let movies = JSON.parse(xhr.responseText);
+        xhr.addEventListener("load", () => {
+            let data = JSON.parse(xhr.responseText);
 
-        movies.forEach(m => {
-            let div = document.createElement("div");
-            div.innerText = m.title;
-            output.appendChild(div);
+            observer.next(data);
+            observer.complete();
         });
-    });
 
-    xhr.open("GET", url);
-    xhr.send();
+        xhr.open("GET", url);
+        xhr.send();
+    });
 }
 
-click.subscribe(
-    e => load("movies.json"),
+function renderMovies(movies: { title: string }[]) {
+    output.innerHTML = null;
+    movies.forEach(m => {
+        let div = document.createElement("div");
+        div.innerText = m.title;
+        output.appendChild(div);
+    });
+}
+
+click.flatMap(e => load("movies.json"))
+    .subscribe(
+    renderMovies,
     error => console.log(`error: ${error}`),
     () => console.log('complete')
-);
+    );
+
+//SUBSCRIBE INSIDE OTHER SUBSCRIBE IS POSSIBLE BUT NOT THE BEST WAY -> USE FLATMAP OPERATOR INSTEAD
+// click.subscribe(
+//     e => load("movies.json").subscribe(renderMovies),
+//     error => console.log(`error: ${error}`),
+//     () => console.log('complete')
+// );
